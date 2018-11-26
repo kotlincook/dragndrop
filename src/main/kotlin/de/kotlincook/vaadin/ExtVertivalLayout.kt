5e3f4f19a9fd3299e2788cb15b416516086a2608
javacook <javacook@gmx.de>
2018-11-27 00:21:57 +0100

    insert mit vararg

--- src/main/kotlin/de/kotlincook/vaadin/ExtVertivalLayout.kt
+++ src/main/kotlin/de/kotlincook/vaadin/ExtVertivalLayout.kt
@@ -7,27 +7,30 @@ open class ExtVertivalLayout: VerticalLayout() {
 
     val components: MutableList<Component> = mutableListOf()
 
-    override fun add(vararg components: Component) {
-        super.add(*components)
-        this.components.addAll(components)
+    override fun add(vararg compsToAdd: Component) {
+        super.add(*compsToAdd)
+        this.components.addAll(compsToAdd)
     }
 
-    fun insert(pos: Int, component: Component) {
-        require(pos in 0 until components.size) {
-            "Value of pos = $pos is not in '0 until ${components.size}'"
-        }
-        add(component)
-//        components.add(component)
-        for (i in components.lastIndex downTo pos+1) {
-            swapComponents(i)
-        }
-    }
+    fun insert(pos: Int, vararg compsToInsert: Component) {
 
-    private fun swapComponents(i: Int) {
-        replace(components[i - 1], components[i])
-        val temp = components[i - 1]
-        components[i - 1] = components[i]
-        components[i] = temp
+        fun swapComponents(i: Int) {
+            replace(components[i - 1], components[i])
+            val temp = components[i - 1]
+            components[i - 1] = components[i]
+            components[i] = temp
+        }
+
+        require(pos in 0..components.size) {
+            "Value of pos = $pos is not in '0..${components.size}'"
+        }
+        val prevComponentsSize = components.size
+        add(*compsToInsert)
+        for (i in prevComponentsSize downTo pos + 1) {
+            for (j in i until i + compsToInsert.size) {
+                swapComponents(j)
+            }
+        }
     }
 
 }
